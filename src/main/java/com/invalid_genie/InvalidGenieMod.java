62f48b2d547efde75756129499347ac9dad868b2
AlmerMinecraft <nicitapaklin@yandex.ru>
2023-12-09 19:13:40 +0500

    Initial commit

---
+++ src/main/java/com/invalid_genie/InvalidGenieMod.java
@@ -0,0 +1,15 @@
+package com.invalid_genie;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class InvalidGenieMod implements ModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("invalid_genie");
+	public static final String MOD_ID = "invalid_genie";
+
+	@Override
+	public void onInitialize() {
+	}
+}
